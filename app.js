595b836ceea333cef46b2054bff3c54faa4bfb1e
musango willy <musangowilly@gmail.com>
2023-10-16 23:17:14 +0300

    nice

---
+++ app.js
@@ -0,0 +1,28 @@
+const  theScore=50;
+
+
+if(theScore<=39){
+
+    console.log("John failed the test,is a F9 student");
+
+}else if(theScore>=40 && theScore<=49){
+
+    console.log("John is a D student");
+
+}else if (theScore>=50 && theScore<=59){
+
+    console.log("John is a C student");
+
+}else if(theScore>=60 && theScore<=69){
+
+    console.log("John is a B student");
+
+}else if(theScore>=70){
+
+    console.log("John is a A student");
+
+}else{//negative score
+
+    console.log("no score");
+
+}
