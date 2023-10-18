a6dba65c3d1a9abfb2ae2d6a533f734227ad0225
musango willy <musangowilly@gmail.com>
2023-10-18 22:59:43 +0300

    well

--- function.js
+++ function.js
@@ -1,9 +1,19 @@
 
 function sum(num1,num2,num3){
+
     const sum = num1+  num2 + num3;
+
     console.log(sum);
+
+    return sum;
+
+    console.log ("sorry won't print,because it meets return before me");
 } 
 
 Number()
 
-const add=sum(20,20,10)//50
+const add=sum(20,20,10);//50
+
+console.log(sum);
+
+
