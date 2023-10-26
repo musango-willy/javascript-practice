b17e0d572e69c41602614c2c53eb506d312d9dcb
musango willy <musangowilly@gmail.com>
2023-10-26 23:51:24 +0300

    kop

---
+++ array.js
@@ -0,0 +1,12 @@
+// multidimensional arrays
+
+const data=[[1,2,3],[3,5,5],[5,6,7]]
+
+console.log(data[0][0]);
+
+const data=[
+    ["peter",42],
+    ["john",24],
+    ["paul",45],
+]
+console.log(data[0][0]);
