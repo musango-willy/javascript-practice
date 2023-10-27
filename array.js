ca76fc22932c1922ff8f7df80fd7bed6a71da057
musango willy <musangowilly@gmail.com>
2023-10-27 11:33:54 +0300

    uuh

--- array.js
+++ array.js
@@ -17,3 +17,13 @@ for (let i =0;i<studentData.length;i++){
         console.log(studentData[i][j])
     }
 }
+
+const numbers =[1,2,3,4,5,6,7]
+
+let sum=0
+
+for (let i=0;i<numbers.length;i++){
+    
+    sum+=numbers[i]
+}
+console.log(sum)
