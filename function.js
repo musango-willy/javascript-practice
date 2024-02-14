f574653df2998d9f7c8249a50ae12f0a8307e894
musango willy <musangowilly@gmail.com>
2024-02-14 21:44:01 +0300

    learning fetch method

--- function.js
+++ function.js
@@ -18,5 +18,8 @@ const add=sum(20,20,10);//50 it is used to add two or more operands
 
 console.log(sum);
 
+//fetching data from database
 
-
+fetch('https://dummyjson.com/product/2')
+.then(res=>res.json())
+.then(json=>console.log(json))
